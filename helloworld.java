255193872826db3a0a8b66fbd17bd77d753308f5
Aziz Ramazanov <85971989+Aziz186@users.noreply.github.com>
2022-03-22 19:37:37 +0100

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World. My name is Aziz Ramazanov and my student id is 52708. );
+    }
+
+}
